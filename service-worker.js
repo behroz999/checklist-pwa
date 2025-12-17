51ed07eae60d05c7491f911ed54466048f16620b
behroz999 <134800915+behroz999@users.noreply.github.com>
2025-12-17 20:57:57 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,10 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+const cacheName = 'checklist-cache-v1';
+const filesToCache = ['./index.html','./app.js','./manifest.json','./icon.png'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
